db4bb55ce0af82590b102bfdf54ec856203c9368
blackboytechie <138457971+Blackboytechie@users.noreply.github.com>
2024-04-05 17:35:39 +0530

    added user return response while adding address

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.listen(PORT, () => {
 
 // Endpoint to store a new address
 app.post("/addaddress",async(req,res)=>{
+  console.log("adding address");
   try {
     const {userId,address} = req.body;
     // find user by the userId
@@ -33,6 +34,7 @@ app.post("/addaddress",async(req,res)=>{
     // add new address to users address array
     user.address.push(address);
     user.save();
+    res.status(200).json({message:"address added successfully"});
   } catch (error) {
     res.status(500).json({message:"Error Adding Address"})
   }
