3cd8651dbcc843b80cf3cb5b68c0cf320070ebf7
blackboytechie <138457971+Blackboytechie@users.noreply.github.com>
2024-04-09 22:11:29 +0530

    edited paymentsheet to handle customerid

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const authRouter = require("./routes/auth.js");
 const cors = require("cors");
 const UserModel = require("./models/user.js");
 const Order = require("./models/order.js");
-const stripe = require('stripe')('sk_test_51P3HfZSIOfadBdWaYyVJCvhFKNZjySjX8Qz8oPNOxkEuFSYX9ubjruAlL8OOnw6TYY6tLNV4I2bjyaVj054Mv9Ck00TTUDh3kb');
+const stripe = require("stripe")(
+  "sk_test_51P3HfZSIOfadBdWaYyVJCvhFKNZjySjX8Qz8oPNOxkEuFSYX9ubjruAlL8OOnw6TYY6tLNV4I2bjyaVj054Mv9Ck00TTUDh3kb"
+);
 
 const app = express();
 
@@ -16,7 +18,7 @@ app.use("/auth", authRouter);
 // app.use("/stripe", stripeRouter);
 
 app.get("/", (req, res) => {
-  res.json({ shop:"mahinelectrical" });
+  res.json({ shop: "mahinelectrical" });
 });
 
 const PORT = process.env.PORT || 8000;
@@ -26,102 +28,139 @@ app.listen(PORT, () => {
 });
 
 // Endpoint to store a new address
-app.post("/addaddress",async(req,res)=>{
+app.post("/addaddress", async (req, res) => {
   console.log("adding address");
   try {
-    const {userId,address} = req.body;
+    const { userId, address } = req.body;
     // find user by the userId
     const user = await UserModel.findById(userId);
-    if(!user){
-      return res.status(404).json({message:"User Not Found"})
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
     }
     // add new address to users address array
     user.address.push(address);
     user.save();
-    res.status(200).json({message:"address added successfully"});
+    res.status(200).json({ message: "address added successfully" });
   } catch (error) {
-    res.status(500).json({message:"Error Adding Address"})
+    res.status(500).json({ message: "Error Adding Address" });
   }
-})
+});
 
 //Endpoint to get all addresses of the user
-app.get("/addresses/:userId",async(req,res)=>{
+app.get("/addresses/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
     const user = await UserModel.findById(userId);
-    if(!user){
-      return res.status(404).json({message:"User Not Found"})
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
     }
     const addresses = user.address;
-    res.status(200).json({message:"ok",addresses})
+    res.status(200).json({ message: "ok", addresses });
   } catch (error) {
-    res.status(500).json({message:"Error getting User Addresses"})
+    res.status(500).json({ message: "Error getting User Addresses" });
   }
-})
+});
 
 //Endpoint To Store All The Orders
-app.post("/placeorder",async(req,res)=>{
+app.post("/placeorder", async (req, res) => {
   try {
-    const {userId,cartItems,totalPrice,shippingAddress,paymentMethod}=req.body;
+    const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } =
+      req.body;
     console.log("userId: " + userId);
     console.log("cartItems: " + cartItems);
     console.log("totalPrice: " + totalPrice);
     console.log("shippingAddress: " + shippingAddress);
     console.log("paymentMethod: " + paymentMethod);
     const user = await UserModel.findById(userId);
-    if(!user){
+    if (!user) {
       console.log("user not found");
-      return res.status(404).json({message:"user not found"});
+      return res.status(404).json({ message: "user not found" });
     }
     // create array of products from cart items
-    const products = cartItems.map((item)=>({
-      name:item?.title,
-      quantity:item?.quantity,
-      price:item?.newPrice,
-      image:item?.image
-    }))
+    const products = cartItems.map((item) => ({
+      name: item?.title,
+      quantity: item?.quantity,
+      price: item?.newPrice,
+      image: item?.image,
+    }));
     console.log("product: " + products);
 
     // create new order
     const order = new Order({
-      user:userId,
-      products:products,
-      totalPrice:totalPrice,
-      shippingAddress:shippingAddress,
-      paymentMethod:paymentMethod
-    })
+      user: userId,
+      products: products,
+      totalPrice: totalPrice,
+      shippingAddress: shippingAddress,
+      paymentMethod: paymentMethod,
+    });
     console.log("placed order: " + order);
     await order.save();
-    res.status(200).json({message:"Order Created Successfully!!!"})
+    res.status(200).json({ message: "Order Created Successfully!!!" });
   } catch (error) {
-    res.status(500).json({message:"Error creating order"})
+    res.status(500).json({ message: "Error creating order" });
   }
-})
+});
 
 //get users orders
-app.get("/orders/:userId",async(req,res) => {
+app.get("/orders/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
-    const orders = await Order.find({user:userId}).populate("user");
-    if(!orders||orders.length === 0) {
-      return res.status(404).json({message:"No Orders Found For This User"})
+    const orders = await Order.find({ user: userId }).populate("user");
+    if (!orders || orders.length === 0) {
+      return res.status(404).json({ message: "No Orders Found For This User" });
     }
-    res.status(200).json({message:"your Order",order:orders})
+    res.status(200).json({ message: "your Order", order: orders });
   } catch (error) {
-    res.status(500).json({message: "Error getting users orders"});
+    res.status(500).json({ message: "Error getting users orders" });
   }
-})
+});
 //stripe paymentsheet
-app.post('/payment-sheet', async (req, res) => {
+app.post("/payment-sheet", async (req, res) => {
+  // Get product amount and currency from the request body
+  const { amount, currency, userId } = req.body;
+  // check customerId
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    console.log("user not found");
+    return res.status(404).json({ message: "user not found" });
+  }
+  if(user.customerId){
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customerId },
+      { apiVersion: "2023-10-16" }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      customer: customerId,
+      // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+      // is optional because Stripe enables its functionality by default.
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    // Return the paymentIntent to the client
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey:
+        "pk_test_51P3HfZSIOfadBdWaO7w2Fs0Zo49SwMd929OMWKFnTLtngJkZ9MZQ0kFgBrAG3r5pPWD0aOTLxLCWr5aQxyvYrz2E00WajNJ0gp",
+    });
+  }
+
   // Use an existing Customer ID if this is a returning customer.
   const customer = await stripe.customers.create();
+  // save customerId to database
+  user.customerId = customer.id;
+  await user.save();
   const ephemeralKey = await stripe.ephemeralKeys.create(
-    {customer: customer.id},
-    {apiVersion: '2023-10-16'}
+    { customer: customer.id },
+    { apiVersion: "2023-10-16" }
   );
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
+    amount,
+    currency,
     customer: customer.id,
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter
     // is optional because Stripe enables its functionality by default.
@@ -134,6 +173,7 @@ app.post('/payment-sheet', async (req, res) => {
     paymentIntent: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
-    publishableKey: 'pk_test_51P3HfZSIOfadBdWaO7w2Fs0Zo49SwMd929OMWKFnTLtngJkZ9MZQ0kFgBrAG3r5pPWD0aOTLxLCWr5aQxyvYrz2E00WajNJ0gp'
+    publishableKey:
+      "pk_test_51P3HfZSIOfadBdWaO7w2Fs0Zo49SwMd929OMWKFnTLtngJkZ9MZQ0kFgBrAG3r5pPWD0aOTLxLCWr5aQxyvYrz2E00WajNJ0gp",
   });
 });
